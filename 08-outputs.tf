63cecf6ed5d82e2ea9b9184cf5b0ca1bd798f0e2
Andrew Siwko <asiwko@siwko.org>
2026-08-27 17:42:49 -0400

    clean up outputs

--- 08-outputs.tf
+++ 08-outputs.tf
@@ -1,13 +1,3 @@
-output "proxmox_templates" {
-  value = {
-    for vm in data.proxmox_virtual_environment_vms.templates.vms :
-    vm.name => {
-      vm_id = vm.vm_id
-      tags  = vm.tags
-    }
-  }
-}
-
 output "proxmox_vms" {
   value = {
     for vm in proxmox_virtual_environment_vm.vms :
