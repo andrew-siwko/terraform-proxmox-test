5f58dad6a95057e8a4c9de55473306d39376d426
Andrew Siwko <asiwko@siwko.org>
2026-08-27 09:54:51 -0400

    add a second smaller machine

--- 06-servers.tf
+++ 06-servers.tf
@@ -1,7 +1,7 @@
-resource "proxmox_virtual_environment_vm" "test_vm" {
+resource "proxmox_virtual_environment_vm" "test_vm1" {
   name      = "proxmox-01"
   node_name = "proxmox"
-  vm_id     = 200
+  vm_id     = 201
 
   agent {
     enabled = true
@@ -44,7 +44,53 @@ resource "proxmox_virtual_environment_vm" "test_vm" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "test_vm2" {
+  name      = "proxmox-02"
+  node_name = "proxmox"
+  vm_id     = 202
+
+  agent {
+    enabled = true
+  }
+
+  clone {
+    vm_id = 9002
+    full  = true
+  }
+
+  cpu {
+    cores = 3
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 10
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+    user_account {
+      username = "asiwko"
+      keys     = [file(var.ssh_public_key_path)]
+    }
+  }
+}
+
 resource "time_sleep" "wait_for_network" {
-  depends_on      = [proxmox_virtual_environment_vm.test_vm]
+  depends_on      = [proxmox_virtual_environment_vm.test_vm1,proxmox_virtual_environment_vm.test_vm2]
   create_duration = "30s"
 }
