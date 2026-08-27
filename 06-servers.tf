225e8bdaa0afcb7f98b6ce9ad3c6e1fb08cd27e6
Andrew Siwko <asiwko@siwko.org>
2026-08-27 10:31:30 -0400

    rewrite with a loop and remove timeer

--- 06-servers.tf
+++ 06-servers.tf
@@ -1,41 +1,45 @@
-resource "proxmox_virtual_environment_vm" "test_vm1" {
-  name      = "proxmox-01"
-  node_name = "proxmox"
-  vm_id     = 201
-
-  agent {
-    enabled = true
+locals {
+  vms = {
+    "prox01" = { id = 201, clone_id = 9001, cores = 3, memory = 2048, disk = 30 }
+    "prox02" = { id = 202, clone_id = 9002, cores = 3, memory = 1024, disk = 10 }
   }
+}
 
+# 1. Provision VMs using for_each to avoid code duplication
+resource "proxmox_virtual_environment_vm" "vms" {
+  for_each  = local.vms
+  name      = "proxmox-${substr(each.key, 4, 2)}"
+  node_name = "proxmox"
+  vm_id     = each.value.id
+
+agent {
+    enabled = true
+    wait    = true      # Blocks resource creation until Guest Agent responds
+    timeout = "3m"      # Maximum time to wait before failing (e.g. 3 minutes)
+  }
   clone {
-    vm_id = 9001
+    vm_id = each.value.clone_id
     full  = true
   }
 
   cpu {
-    cores = 3
+    cores = each.value.cores
     type  = "host"
   }
 
-  memory {
-    dedicated = 2048
-  }
+  memory { dedicated = each.value.memory }
 
   disk {
     datastore_id = "local-lvm"
     interface    = "scsi0"
-    size         = 30
+    size         = each.value.disk
   }
 
-  network_device {
-    bridge = "vmbr0"
-  }
+  network_device { bridge = "vmbr0" }
 
   initialization {
     ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
+      ipv4 { address = "dhcp" }
     }
     user_account {
       username = "asiwko"
@@ -44,53 +48,4 @@ resource "proxmox_virtual_environment_vm" "test_vm1" {
   }
 }
 
-resource "proxmox_virtual_environment_vm" "test_vm2" {
-  name      = "proxmox-02"
-  node_name = "proxmox"
-  vm_id     = 202
 
-  agent {
-    enabled = true
-  }
-
-  clone {
-    vm_id = 9002
-    full  = true
-  }
-
-  cpu {
-    cores = 3
-    type  = "host"
-  }
-
-  memory {
-    dedicated = 1024
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    interface    = "scsi0"
-    size         = 10
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-    user_account {
-      username = "asiwko"
-      keys     = [file(var.ssh_public_key_path)]
-    }
-  }
-}
-
-resource "time_sleep" "wait_for_network" {
-  depends_on      = [proxmox_virtual_environment_vm.test_vm1,proxmox_virtual_environment_vm.test_vm2]
-  create_duration = "30s"
-}
