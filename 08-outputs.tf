3084eb608a9cd96156cd1f1041706e756f85ea81
Andrew Siwko <asiwko@siwko.org>
2026-09-17 10:43:02 -0400

    add knodes to the output

--- 08-outputs.tf
+++ 08-outputs.tf
@@ -1,3 +1,14 @@
+output "knode_vms" {
+  value = {
+    for vm in proxmox_virtual_environment_vm.knodes :
+    vm.name => {
+      id = vm.id
+      node_name  = vm.node_name
+      ipv4_addresses = vm.ipv4_addresses
+    }
+  }
+}
+
 output "proxmox_vms" {
   value = {
     for vm in proxmox_virtual_environment_vm.vms :
