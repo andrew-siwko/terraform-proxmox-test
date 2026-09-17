b8ff00c12875e3816beb41862aac5ddb6b3f30a9
Andrew Siwko <asiwko@siwko.org>
2026-09-17 10:27:28 -0400

    updated domain registraitions.

--- 07-domain.tf
+++ 07-domain.tf
@@ -22,7 +22,7 @@ resource "linode_domain" "dns_zone" {
 # }
 
 resource "linode_domain_record" "a_records" {
-  for_each    = local.vms
+  for_each    = local.prox_vms
   domain_id   = linode_domain.dns_zone.id
   name        = each.key
   record_type = "A"
@@ -32,6 +32,24 @@ resource "linode_domain_record" "a_records" {
   target = coalesce(
     one([
       for ip in flatten(proxmox_virtual_environment_vm.vms[each.key].ipv4_addresses) :
+      ip if ip != "127.0.0.1" && !startswith(ip, "169.254.")
+    ]),
+    "127.0.0.1"
+  )
+  # depends_on = [time_sleep.wait_for_dhcp]
+}
+
+resource "linode_domain_record" "knode_a_records_knode" {
+  for_each    = local.knode_vms
+  domain_id   = linode_domain.dns_zone.id
+  name        = each.key
+  record_type = "A"
+  ttl_sec     = 5
+
+  # Safely extracts the first non-loopback, non-link-local IPv4 address
+  target = coalesce(
+    one([
+      for ip in flatten(proxmox_virtual_environment_vm.knodes[each.key].ipv4_addresses) :
       ip if ip != "127.0.0.1" && !startswith(ip, "169.254.")
     ]),
     "127.0.0.1"
